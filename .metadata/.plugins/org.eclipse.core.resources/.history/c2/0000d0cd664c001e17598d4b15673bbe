152594b0fef4dfdd5642dab3b54eec50a5295062
Jeon Seung Min <jeonsm6@gmail.com>
2023-09-06 16:07:53 +0900

    09.06 스프링 수업 MyBatis 연동 및 커넥션 풀 연동 오늘도 고생했다 !!

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/c2/0000d0cd664c001e17598d4b15673bbe
@@ -0,0 +1,10 @@
+package org.galapagos.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration	// 설정 파일이니 분석해줘 스프링 !
+@ComponentScan(basePackages = {"org.galapagos.sample"})
+public class RootConfig {
+
+}
--- ex00/src/main/java/org/galapagos/config/RootConfig.java
+++ ex00/src/main/java/org/galapagos/config/RootConfig.java
@@ -1,10 +1,43 @@
 package org.galapagos.config;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration	// 설정 파일이니 분석해줘 스프링 !
-@ComponentScan(basePackages = {"org.galapagos.sample"})
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration // 설정 파일이니 분석해줘 스프링 !
+@ComponentScan(basePackages = { "org.galapagos.sample" })
+@MapperScan(basePackages = { "org.galapagos.mapper" })
 public class RootConfig {
+
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+//		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		config.setJdbcUrl("jdbc:mysql://localhost:3307/glory_db");
+		
+		config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		config.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3307/glory_db");
+		
+		config.setUsername("GLORY");
+		config.setPassword("1234");
+		HikariDataSource dataSource = new HikariDataSource(config);
+		return dataSource;
+	}
+	
+	// MyBatis 핵심 설정
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
 
 }
